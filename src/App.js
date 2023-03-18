973138b4411ec759d3fc1ac025277400bbc661b9
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-18 18:15:57 +0100

    added seccond route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 // https://example.com/products   path is the part after domain
 
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+const router = createBrowserRouter([
+	{ path: '/', element: <HomePage /> },
+	{ path: '/products', element: <ProductsPage /> },
+]);
 
 function App() {
 	return <RouterProvider router={router} />;
