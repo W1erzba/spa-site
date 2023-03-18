022f8aed2ed4b4d1e5a8f09f2384447624da2bbe
[Bartosz Wierzbicki] <101505041+W1erzba@users.noreply.github.com>
2023-03-18 18:46:28 +0100

    added navigation between pages and added a RootLayout by Root from redux-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	// createRoutesFromElements,
+	// Route,
+} from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import { RootLayout } from './pages/Root';
+
+// const routDefinitions = createRoutesFromElements(
+// 	<Route>
+// 		<Route path='/' element={<HomePage />}></Route>,
+// 		<Route path='/products' element={<ProductsPage />}></Route>
+// 	</Route>
+// );
+// const router = createBrowserRouter(routDefinitions);
 
 // https://example.com/products   path is the part after domain
 
 const router = createBrowserRouter([
-	{ path: '/', element: <HomePage /> },
-	{ path: '/products', element: <ProductsPage /> },
+	{
+		path: '/',
+		element: <RootLayout />,
+		children: [
+			{ path: '/', element: <HomePage /> },
+			{ path: '/products', element: <ProductsPage /> },
+		],
+	},
 ]);
 
 function App() {
